16c49e32ff9d48beb90a75d9cd3351d709a1f3ab
ecruzx01 <cafeexpreso@gmail.com>
2026-03-31 08:00:10 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'aquila46-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve la versión guardada en el teléfono si no hay internet
+        return response || fetch(event.request);
+      })
+  );
 });
